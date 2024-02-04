3810f518cffeda30e3e9a8ef27a482fe9b5f682c
Cristian Nass <cristian.tirdad@gmail.com>
2024-02-05 00:15:01 +0100

    get Data

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
-import Image from "next/image";
+export default async function Home() {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movies/popular?api_key=${process.env.API_KEY}`
+  );
 
-export default function Home() {
-  return <main>Wellcome to Nextjs App</main>;
+  console.log("--------->", data.json());
+
+  return (
+    <main>
+      <div>Wellcome to Nextjs App</div>
+    </main>
+  );
 }
